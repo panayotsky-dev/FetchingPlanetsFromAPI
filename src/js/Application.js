701626d3093eb5919263fe0022cf0bbcf8d9f293
panayotsky.dev <panayotsky.dev@gmail.com>
2023-01-19 02:04:23 +0200

    solution

--- src/js/Application.js
+++ src/js/Application.js
@@ -7,24 +7,96 @@ export default class Application extends EventEmitter {
       READY: "ready",
     };
   }
-
+//  https://swapi.boom.dev/api/planets
   constructor() {
-    super();
+    super(); 
+    this._loading = document.querySelector(".progress");
+     this._startLoading()
+    
 
-    const box = document.createElement("div");
-    box.classList.add("box");
-    box.innerHTML = this._render({
-      name: "Placeholder",
-      terrain: "placeholder",
-      population: 0,
-    });
-
-    document.body.querySelector(".main").appendChild(box);
+    
 
     this.emit(Application.events.READY);
   }
+  _startLoading(){
+    this._loading.style.visibility = "visible"; 
+    this._load();
+  }
+  async _load(){     
+      
+    let fetchedPlanets = [];    
+    let API_URL = 'https://swapi.boom.dev/api/planets'
+    
+    //fetching initial api url ( first page)
+    fetch(API_URL)
+      .then(response => response.json())
+      .then((data) => {        
+        data.results.forEach(element => {
+          fetchedPlanets.push(element)
+          this._create(element)
+        })     
+      })  
+      
+    // fetching other pages of the api
+    for(let i =2;i<=6;i++){
+      API_URL = 'https://swapi.boom.dev/api/planets?page=' + i;
+      fetch(API_URL)
+      .then(response => response.json())
+      .then((data) => {
+        data.results.forEach(element => {
+          fetchedPlanets.push(element)
+          this._create(element)
+         
+        })    
+        
+      }) 
+      
+    } 
+  //   Object.keys
+  
+    // fetchedPlanets.forEach(({name, terrain, population}) => console.log(name,terrain,population));
+    this._stopLoading()
+    //v2
+    // const API_URL = "https://swapi.boom.dev/api/planets"
+    // let planets = [];
+    // const response = await fetch(API_URL)
+    // let {next, results } = await response.json();
 
-  _render({ name, terrain, population }) {
+    // planets = [...results]
+    
+    // while (next !== null) {
+    //   const response = await fetch(next)
+    //   const result = await response.json();
+    //   planets = [...planets, ...result.results]
+    //   next = result.next;
+    // }
+
+    // planets.forEach(({name, terrain, population}) => this._create(name,terrain,population) )
+    // this._stopLoading();
+
+  }
+ 
+  _stopLoading(){
+    this._loading.style.visibility = "hidden"; 
+  }
+_create({name,terrain,population}){
+    
+    let box = document.createElement('div')
+    box.classList.add('box')
+      box.innerHTML = this._render({
+        name:name,
+        terrain:terrain,
+        population:population,
+      })
+    document.querySelector('.main').appendChild(box)
+}
+
+
+
+
+
+
+  _render({ name, terrain, population }) {      
     return `
 <article class="media">
   <div class="media-left">
